376516f18c9c0b4040f835ec33990b8c107cf0b0
Yael Hazam <Yaelhazam@gmail.com>
2025-01-17 18:20:37 +0200

    blabla

--- main.cpp
+++ main.cpp
@@ -5,19 +5,27 @@
 #include "Player.hpp"
 #include "AiPlayer.hpp"
 #include "Game.hpp"
+#include "Grid.hpp"
+#include "Ship.hpp"
+#include "ShipPosition.hpp"
+#include "Destroyer.hpp"
+#include "Cruiser.hpp"
+#include "Carrier.hpp"
+#include "Submarine.hpp"
+#include "BattleShip.hpp"
 
 int main()
 {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
-   
+
     Player *p1 = new HumanPlayer();
     Player *p2 = new AiPlayer();
 
     Game game(p1, p2);
 
     game.setup();
-    
+
     game.start();
-    
+
     return 0;
 }
